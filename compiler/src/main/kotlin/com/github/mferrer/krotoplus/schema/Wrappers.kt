913be1a5bc7a7535d92227b7b83ede58f6c895b9
marcoferrer <marrferr.cdx@gmail.com>
2018-03-13 23:39:11 -0400

    initial commit

---
+++ compiler/src/main/kotlin/com/github/mferrer/krotoplus/schema/Wrappers.kt
@@ -0,0 +1,40 @@
+package com.github.mferrer.krotoplus.schema
+
+import com.squareup.kotlinpoet.*
+import com.squareup.wire.schema.ProtoFile
+import com.squareup.wire.schema.Rpc
+import com.squareup.wire.schema.Schema
+import com.squareup.wire.schema.Service
+
+data class ServiceWrapper(val service: Service, val protoFile: ProtoFile, val protoSchema: Schema) {
+
+    val name: String get() = service.name()
+
+    val enclosingServiceClassName = ClassName(protoFile.javaPackage(), "${name}Grpc")
+
+    val asyncStubClassName = ClassName(enclosingServiceClassName.canonicalName, "${name}Stub")
+
+    val futureStubClassName = ClassName(enclosingServiceClassName.canonicalName, "${name}FutureStub")
+
+    val blockingStubClassName = ClassName(enclosingServiceClassName.canonicalName, "${name}BlockingStub")
+
+    val methodDefinitions = service.rpcs().map { MethodWrapper(it) }
+
+    inner class MethodWrapper(val method: Rpc) {
+
+        val functionName = method.name().decapitalize()
+
+        val serviceWrapper: ServiceWrapper get() = this@ServiceWrapper
+
+        val requestClassName = method.requestType().toClassName(serviceWrapper.protoSchema)
+
+        val responseClassName = method.responseType().toClassName(serviceWrapper.protoSchema)
+
+        val methodDescriptorName = "METHOD" + method.name().replace(wordStartCamelCaseRegex,"_$1").toUpperCase()
+
+    }
+
+    companion object {
+        val wordStartCamelCaseRegex = "([A-Z])(?=[a-z])".toRegex()
+    }
+}
