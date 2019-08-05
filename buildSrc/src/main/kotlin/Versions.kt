4da8aaf59151f4cb8001e9685acef487696a993e
Marco Ferrer <35935108+marcoferrer@users.noreply.github.com>
2019-08-04 21:48:59 -0400

    downgrade grpc version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val protobuf = "3.9.0"
-    const val grpc = "1.22.1"
+    const val grpc = "1.19.0"
     const val kotlin = "1.3.41"
     const val coroutines = "1.3.0-RC"
     const val mockk = "1.9.1"
