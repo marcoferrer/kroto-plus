76d0433138d3c6c7c9d1fd82e7a9b0fc951f0a60
okue <nogideca@gmail.com>
2020-09-13 14:15:37 +0900

    Bump up dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 object Versions {
-    const val protobuf = "3.9.0"
+    const val protobuf = "3.13.0"
     const val commonProto = "1.16.0"
-    const val grpc = "1.23.0"
-    const val kotlin = "1.3.61"
-    const val coroutines = "1.3.3"
-    const val mockk = "1.9.3"
+    const val grpc = "1.32.1"
+    const val kotlin = "1.4.10"
+    const val coroutines = "1.3.9"
+    const val mockk = "1.10.0"
 }
